9964c4b491dd9a821e2ccef0530a95b8a2a8f2b9
kelvin beno <kelvinbeno526@gmail.com>
2019-02-14 12:56:19 +0300

    commited five

--- tests/v1/test_parties.py
+++ tests/v1/test_parties.py
@@ -1,11 +1,11 @@
 import json
 import unittest
-from run import create_app
+from run import app
 
 
 class TestPoliticoApp(unittest.TestCase):
     def setUp(self):
-        self.app = create_app()
+        self.app = app()
         self.client = self.app.test_client()
         self.data = {
             "name": "Kelvin",
